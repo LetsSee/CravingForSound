97e53706154432b30090428ed92a59ea861d9a17
rodion <rodion.bychkov@gmail.com>
2019-11-05 00:53:25 +0300

    generic request

--- Net/Net/NetworkService.swift
+++ Net/Net/NetworkService.swift
@@ -44,74 +44,14 @@ public final class NetworkService: NetworkServiceProtocol {
     public func artistSearch(by name: String) -> Single<[Artist]> {
         let string = "/2.0/?method=artist.search&artist=\(name)&api_key=\(key)&format=json"
         
-        NetworkMonitor.shared.requestStarted()
-        
-        guard NetworkMonitor.shared.isNetworkReachable else {
-            return .error(NetworkServiceError.unreachable)
-        }
-        
-        guard let baseUrl = URL(string: baseUrl) else {
-            return .error(NetworkServiceError.badBaseUrl)
-        }
-        
-        guard let url = URL(string: string, relativeTo: baseUrl) else {
-            return .error(NetworkServiceError.badMethodUrl)
-        }
-        
-        if debug { print("loading: \(url.absoluteString)") }
-        
-        return Single<[Artist]>.create { observer in
-            data(.get, url)
-                .observeOn(MainScheduler.instance)
-                .subscribe(onNext: { data in
-                    NetworkMonitor.shared.requestFinished()
-                    do {
-                        let decoder = JSONDecoder()
-                        let result = try decoder.decode(ArtistsResponse.self, from: data)
-                        observer(.success(result.artists))
-                    } catch {
-                        observer(.error(NetworkServiceError.badFormat))
-                    }
-                }, onError: { error in
-                    observer(.error(NetworkServiceError.networkError))
-                })
-        }
+        return request(with: string, parseType: ArtistsResponse.self).map { $0.artists }
     }
     
     public func topAlbums(by mbid: String) -> Single<[Album]> {
         let string = "/2.0/?method=artist.gettopalbums&mbid=\(mbid)&api_key=\(key)&format=json"
         
-        NetworkMonitor.shared.requestStarted()
-        
-        guard NetworkMonitor.shared.isNetworkReachable else {
-            return .error(NetworkServiceError.unreachable)
-        }
-        
-        guard let baseUrl = URL(string: baseUrl) else {
-            return .error(NetworkServiceError.badBaseUrl)
-        }
-        
-        guard let url = URL(string: string, relativeTo: baseUrl) else {
-            return .error(NetworkServiceError.badMethodUrl)
-        }
-        
-        if debug { print("loading: \(url.absoluteString)") }
-        
-        return Single<[Album]>.create { observer in
-            data(.get, url)
-                .observeOn(MainScheduler.instance)
-                .subscribe(onNext: { data in
-                    NetworkMonitor.shared.requestFinished()
-                    do {
-                        let decoder = JSONDecoder()
-                        let result = try decoder.decode(AlbumsResponse.self, from: data)
-                        observer(.success(result.albums.filter { $0.mbid != nil }))
-                    } catch {
-                        observer(.error(NetworkServiceError.badFormat))
-                    }
-                }, onError: { error in
-                    observer(.error(NetworkServiceError.networkError))
-                })
+        return request(with: string, parseType: AlbumsResponse.self).map { result in
+            result.albums.filter { $0.mbid != nil }
         }
     }
     
@@ -120,6 +60,10 @@ public final class NetworkService: NetworkServiceProtocol {
         let mbid = mbid ?? ""
         let string = "/2.0/?method=album.getinfo&mbid=\(mbid)&artist=\(name)&api_key=\(key)&format=json"
         
+        return request(with: string, parseType: AlbumResponse.self).map { $0.album }
+    }
+    
+    private func request<T: Decodable>(with string: String, parseType: T.Type) -> Single<T> {
         NetworkMonitor.shared.requestStarted()
         
         guard NetworkMonitor.shared.isNetworkReachable else {
@@ -136,15 +80,15 @@ public final class NetworkService: NetworkServiceProtocol {
         
         if debug { print("loading: \(url.absoluteString)") }
         
-        return Single<AlbumDetails>.create { observer in
+        return Single<T>.create { observer in
             data(.get, url)
                 .observeOn(MainScheduler.instance)
                 .subscribe(onNext: { data in
                     NetworkMonitor.shared.requestFinished()
                     do {
                         let decoder = JSONDecoder()
-                        let result = try decoder.decode(AlbumResponse.self, from: data)
-                        observer(.success(result.album))
+                        let result = try decoder.decode(T.self, from: data)
+                        observer(.success(result))
                     } catch {
                         observer(.error(NetworkServiceError.badFormat))
                     }
